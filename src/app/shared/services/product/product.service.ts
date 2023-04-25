02cf1098ee7828d2e0a3e8069d4ccce8be775385
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-04-25 17:42:54 +0300

    update

--- src/app/shared/services/product/product.service.ts
+++ src/app/shared/services/product/product.service.ts
@@ -1,38 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IProductResponse, IProductRequest } from '../../interfaces/product/product.interface';
+import { IProductRequest, IProductResponse } from '../../interfaces/product/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService implements Resolve<IProductResponse>{
-  public url = environment.BACKEND_URL
-  public api = {products:`${this.url}/products`}
-  constructor(
-    private http:HttpClient
-  ) { }
-  getAll():Observable<IProductResponse[]>{
-    return this.http.get<IProductResponse[]>(this.api.products)
+export class ProductService {
+
+  private url = environment.BACKEND_URL;
+  private api = { products: `${this.url}/products` };
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<IProductResponse[]> {
+    return this.http.get<IProductResponse[]>(this.api.products);
   }
-  getAllByCategore(category:string):Observable<IProductResponse[]>{
-    return this.http.get<IProductResponse[]>(`${this.api.products}?category.path=${category}`)
+
+  getAllByCategory(name: string): Observable<IProductResponse[]> {
+    return this.http.get<IProductResponse[]>(`${this.api.products}?category.path=${name}`);
   }
-  getOne(id:number):Observable<IProductResponse>{
-    return this.http.get<IProductResponse>(`${this.api.products}/${id}`)
+
+  getOne(id: number): Observable<IProductResponse> {
+    return this.http.get<IProductResponse>(`${this.api.products}/${id}`);
   }
-  create(products:IProductRequest):Observable<IProductResponse>{
-    return this.http.post<IProductResponse>(this.api.products, products)
-    }
-    update(products:IProductRequest, id:number):Observable<IProductResponse>{
-      return this.http.patch<IProductResponse>(`${this.api.products}/${id}`,products)
-    }
-    delete(id:number):Observable<void>{
-      return this.http.delete<void>(`${this.api.products}/${id}`)
-     }
-     resolve(act:ActivatedRouteSnapshot): Observable<IProductResponse>{
-      return this.http.get<IProductResponse>(`${this.api.products}/${act.paramMap.get('id')}`)
-     }
+
+  create(product: IProductRequest): Observable<IProductResponse> {
+    return this.http.post<IProductResponse>(this.api.products, product);
+  }
+
+  update(product: IProductRequest, id: number): Observable<IProductResponse> {
+    return this.http.patch<IProductResponse>(`${this.api.products}/${id}`, product);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.api.products}/${id}`);
+  }
+
 }
