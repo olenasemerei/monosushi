8fa6d0c91dd4fab9c5d937156f8cc18d2190ade7
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-04-25 17:46:14 +0300

    update

--- src/app/admin/admin-category/admin-category.component.ts
+++ src/app/admin/admin-category/admin-category.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ICategoryResponse } from '../../shared/interfaces/category/category.interface';
+import { IProductResponse } from '../../shared/interfaces/product/product.interface';
+
+import { ToastrService } from 'ngx-toastr';
 import { CategoryService } from '../../shared/services/category/category.service';
-import { deleteObject, getDownloadURL, percentage, ref, Storage, uploadBytesResumable } from '@angular/fire/storage';
+import { ProductService } from '../../shared/services/product/product.service';
+
+import { ImageService } from '../../shared/services/image/image.service';
 @Component({
   selector: 'app-admin-category',
   templateUrl: './admin-category.component.html',
@@ -20,7 +25,8 @@ export class AdminCategoryComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private categoryService: CategoryService,
-    private storage: Storage
+    private imageService: ImageService,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -32,7 +38,7 @@ export class AdminCategoryComponent implements OnInit {
     this.categoryForm = this.fb.group({
       name: [null, Validators.required],
       path: [null, Validators.required],
-      imagePath: ['https://monosushi.com.ua/wp-content/uploads/2020/10/nav-img-rolls.svg', Validators.required]
+      imagePath: [null, Validators.required]
     });
   }
 
@@ -46,10 +52,12 @@ export class AdminCategoryComponent implements OnInit {
     if(this.editStatus){
       this.categoryService.update(this.categoryForm.value, this.currentCategoryId).subscribe(() => {
         this.loadCategories();
+        this.toastr.success('Category successfully updated');
       })
     } else {
       this.categoryService.create(this.categoryForm.value).subscribe(() => {
         this.loadCategories();
+        this.toastr.success('Category successfully created');
       })
     }
     this.editStatus = false;
@@ -72,12 +80,13 @@ export class AdminCategoryComponent implements OnInit {
   deleteCategory(category: ICategoryResponse): void {
     this.categoryService.delete(category.id).subscribe(() => {
       this.loadCategories();
+      this.toastr.success('Category successfully deleted');
     })
   }
 
   upload(event: any): void {
     const file = event.target.files[0];
-    this.uploadFile('images', file.name, file)
+    this.imageService.uploadFile('images', file.name, file)
       .then(data => {
         this.categoryForm.patchValue({
           imagePath: data
@@ -89,30 +98,8 @@ export class AdminCategoryComponent implements OnInit {
       })
   }
 
-  async uploadFile(folder: string, name: string, file: File | null): Promise<string> {
-    const path = `${folder}/${name}`;
-    let url = '';
-    if(file) {
-      try {
-        const storageRef = ref(this.storage, path);
-        const task = uploadBytesResumable(storageRef, file);
-        percentage(task).subscribe(data => {
-          this.uploadPercent = data.progress
-        });
-        await task;
-        url = await getDownloadURL(storageRef);
-      } catch (e: any) {
-        console.error(e);
-      }
-    } else {
-      console.log('wrong format');
-    }
-    return Promise.resolve(url);
-  }
-
   deleteImage(): void {
-    const task = ref(this.storage, this.valueByControl('imagePath'));
-    deleteObject(task).then(() => {
+    this.imageService.deleteUploadFile(this.valueByControl('imagePath')).then(() => {
       console.log('File deleted');
       this.isUploaded = false;
       this.uploadPercent = 0;
@@ -126,6 +113,4 @@ export class AdminCategoryComponent implements OnInit {
     return this.categoryForm.get(control)?.value;
   }
 
-
 }
-
