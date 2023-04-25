5e6f6f09f043e0135c52a1ef602090aa37e6bb86
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-04-25 13:04:56 +0300

    add files

---
+++ src/app/pages/discount-info/discount-info.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DiscountService } from '../../shared/services/discount/discount.service';
+import { IDiscountResponse } from '../../shared/interfaces/discount/discount.interface';
+
+
+@Component({
+  selector: 'app-discount-info',
+  templateUrl: './discount-info.component.html',
+  styleUrls: ['./discount-info.component.scss']
+})
+export class DiscountInfoComponent implements OnInit {
+
+  public discount!: IDiscountResponse;
+
+  constructor(
+    private discountService: DiscountService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.getOneDiscount();
+  }
+
+  getOneDiscount(): void {
+    const DISCOUNT_ID = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.discountService.getOne(DISCOUNT_ID).subscribe(data => {
+      this.discount = data;
+    })
+  }
+
+}
