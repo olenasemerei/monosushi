ac476dc49e36c686db9954ce9bd88af1a9f5cc06
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-05-01 15:27:40 +0300

    update

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IProductResponse } from '../../shared/interfaces/product/product.interface';
+import { ProductService } from '../../shared/services/product/product.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnDestroy {
+
+  public userProducts: Array<IProductResponse> = [];
+  private eventSubscription!: Subscription;
+
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.eventSubscription = this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        this.loadProducts();
+      }
+    })
+  }
+
+  ngOnInit(): void {}
+
+  loadProducts(): void {
+    const categoryName = this.activatedRoute.snapshot.paramMap.get('category') as string;
+    this.productService.getAllByCategory(categoryName).subscribe(data => {
+      this.userProducts = data;
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.eventSubscription.unsubscribe();
+  }
 
 }
