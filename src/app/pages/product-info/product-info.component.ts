f50aa45e6bf7d9f885f0c2464c99f3deae9ad78b
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-05-01 15:26:37 +0300

    update

--- src/app/pages/product-info/product-info.component.ts
+++ src/app/pages/product-info/product-info.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../shared/services/product/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { IProductResponse } from '../../shared/interfaces/product/product.interface';
+import { OrderService } from '../../shared/services/order/order.service';
 
 @Component({
   selector: 'app-product-info',
   templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.scss']
+  styleUrls: ['./product-info.component.scss'],
 })
-export class ProductInfoComponent {
+export class ProductInfoComponent implements OnInit {
+
+  public currentProduct!: IProductResponse;
+
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private orderService: OrderService
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(response => {
+      this.currentProduct = response['productInfo'];;
+    })
+  }
+
+  loadProduct(): void {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.productService.getOne(id).subscribe(data => {
+      this.currentProduct = data;
+    })
+  }
+
+  productCount(product: IProductResponse, value: boolean): void {
+    if(value){
+      ++product.count;
+    } else if(!value && product.count > 1){
+      --product.count;
+    }
+  }
+
+
+
+  addToBasket(product: IProductResponse): void {
+    let basket: Array<IProductResponse> = [];
+    if(localStorage.length > 0 && localStorage.getItem('basket')){
+      basket = JSON.parse(localStorage.getItem('basket') as string);
+      if(basket.some(prod => prod.id === product.id)){
+        const index = basket.findIndex(prod => prod.id === product.id);
+        basket[index].count += product.count;
+      } else {
+        basket.push(product);
+      }
+    } else {
+      basket.push(product);
+    }
+    localStorage.setItem('basket', JSON.stringify(basket));
+    product.count = 1;
+    this.orderService.changeBasket.next(true);
+  }
 
 }
