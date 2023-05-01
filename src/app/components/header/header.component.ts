143b0487125646983acc180519f7fdcda126c1d4
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-05-01 15:29:16 +0300

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,67 +3,43 @@ import { ROLE } from '../../shared/constants/role.constant';
 import { IProductResponse } from '../../shared/interfaces/product/product.interface';
 import { AccountService } from '../../shared/services/account/account.service';
 import { OrderService } from '../../shared/services/order/order.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent  implements OnInit {
+
   public total = 0;
-  public isLogin = false;
-  public loginUrl = '';
-  public loginPage = '';
-  private basket: IProductResponse[] = [];
+  private basket: Array<IProductResponse> = [];
+
   constructor(
-    private orderService: OrderService,
-    private accountService: AccountService
-  ) {}
+    private orderService: OrderService
+  ) { }
+
   ngOnInit(): void {
     this.loadBasket();
     this.updateBasket();
-    this.checkUserLogin();
-    this.checkUpdateUserLogin();
   }
+
   loadBasket(): void {
-    if (localStorage.length > 0 && localStorage.getItem('basket')) {
+    if(localStorage.length > 0 && localStorage.getItem('basket')){
       this.basket = JSON.parse(localStorage.getItem('basket') as string);
     }
     this.getTotalPrice();
   }
-  getTotalPrice() {
-    this.total = this.basket.reduce(
-      (total: number, prod: IProductResponse) =>
-        total + prod.count * prod.price,
-      0
-    );
-  }
-  updateBasket() {
-    this.orderService.changeBasket.subscribe(() => {
-      this.loadBasket();
-    });
+
+  getTotalPrice(): void {
+    this.total = this.basket
+      .reduce((total: number, prod: IProductResponse) => total + prod.count * prod.price, 0);
   }
 
-  checkUserLogin(): void {
-    const currentUser = JSON.parse(
-      localStorage.getItem('currentUser') as string
-    );
-    if (currentUser && currentUser.role === ROLE.ADMIN) {
-      this.isLogin = true;
-      this.loginUrl = 'admin';
-      this.loginPage = 'Admin';
-    } else if (currentUser && currentUser.role === ROLE.USER) {
-      this.isLogin = true;
-      this.loginUrl = 'cabinet';
-      this.loginPage = 'Cabinet';
-    } else {
-      this.isLogin = false;
-      this.loginUrl = ' ';
-      this.loginPage = '';
-    }
+  updateBasket(): void {
+    this.orderService.changeBasket.subscribe(() => {
+      this.loadBasket();
+    })
   }
-  checkUpdateUserLogin() {
-    this.accountService.isUserLogin$.subscribe(() => {
-      this.checkUserLogin();
-    });
-  }
+
 }
