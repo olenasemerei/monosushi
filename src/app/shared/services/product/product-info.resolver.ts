06d20e4bf4f37e1073f7ce310d3d2b635d86a5a1
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-05-01 15:30:38 +0300

    add files

---
+++ src/app/shared/services/product/product-info.resolver.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IProductResponse } from '../../interfaces/product/product.interface';
+import { ProductService } from './product.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductInfoResolver implements Resolve<IProductResponse> {
+
+  constructor(private productService: ProductService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProductResponse> {
+    return this.productService.getOne(Number(route.paramMap.get('id')));
+  }
+}
+
+
