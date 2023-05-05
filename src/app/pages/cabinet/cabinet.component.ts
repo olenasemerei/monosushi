164b7fc36d888e3449737753a181811bde0b2932
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-05-05 20:31:39 +0300

    Update cabinet.component.ts

--- src/app/pages/cabinet/cabinet.component.ts
+++ src/app/pages/cabinet/cabinet.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/shared/services/account/account.service';
 
 @Component({
   selector: 'app-cabinet',
   templateUrl: './cabinet.component.html',
   styleUrls: ['./cabinet.component.scss']
 })
-export class CabinetComponent {
+export class CabinetComponent implements OnInit {
 
+  constructor(
+    private accountService: AccountService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  signOut(): void {
+    this.router.navigate(['/']);
+    localStorage.removeItem('currentUser');
+    this.accountService.isUserLogin$.next(true);
+  }
 }
+
