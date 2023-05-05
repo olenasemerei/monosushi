7b64cac44f156f85522d7e465273dbbdce8b7827
Olena Semerei <62445851+olenasemerei@users.noreply.github.com>
2023-05-05 20:29:54 +0300

    add files

---
+++ src/app/pages/authorization/authorization.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, UserCredential } from '@angular/fire/auth';
+import { doc, docData, Firestore, setDoc } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { ROLE } from 'src/app/shared/constants/role.constant';
+import { AccountService } from 'src/app/shared/services/account/account.service';
+
+@Component({
+  selector: 'app-authorization',
+  templateUrl: './authorization.component.html',
+  styleUrls: ['./authorization.component.scss']
+})
+export class AuthorizationComponent implements OnInit, OnDestroy {
+
+  public authForm!: FormGroup;
+  public loginSubscription!: Subscription;
+
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router,
+    private auth: Auth,
+    private afs: Firestore,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription.unsubscribe();
+  }
+
+  initAuthForm(): void {
+    this.authForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    })
+  }
+
+  loginUser(): void {
+    const { email, password } = this.authForm.value;
+    this.login(email, password).then(() => {
+      this.toastr.success('User successfully login');
+    }).catch(e => {
+      this.toastr.error(e.message);
+    })
+  }
+
+ async login(email: string, password: string): Promise<void> {
+    const credential = await signInWithEmailAndPassword(this.auth, email, password);
+    this.loginSubscription = docData(doc(this.afs, 'users', credential.user.uid)).subscribe(user => {
+      const currentUser = { ...user, uid: credential.user.uid };
+      localStorage.setItem('currentUser', JSON.stringify(currentUser));
+      if(user && user['role'] === ROLE.USER) {
+        this.router.navigate(['/cabinet']);
+      } else if(user && user['role'] === ROLE.ADMIN){
+        this.router.navigate(['/admin']);
+      }
+      this.accountService.isUserLogin$.next(true);
+    }, (e) => {
+      console.log('error', e);
+    })
+ }
+
+}
